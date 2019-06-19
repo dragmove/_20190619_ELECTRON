946a8092d478e3db798d0a347a3b209eee5c3556
김현석 <dragmove@naver.com>
2019-06-19 14:12:07 +0900

    feat: Add service worker

---
+++ index.js
@@ -0,0 +1,72 @@
+(function() {
+  'use strict';
+
+  const isSupportServiceWorker = 'serviceWorker' in navigator,
+    isSupportMessageChannel = 'MessageChannel' in window;
+
+  // add service worker
+  if (isSupportServiceWorker) {
+    // install service worker
+    navigator.serviceWorker
+      .register('serviceworker.js')
+      .then(function(registration) {
+        console.log('[app] service worker registered with scope :', registration.scope);
+      })
+      .catch(function(err) {
+        console.log('[app] service worker registration failed :', err);
+      });
+
+    navigator.serviceWorker.addEventListener('message', function(event) {
+      console.log('[app] event.data from service worker :', event.data);
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', function(evt) {
+    console.log('[app] DOMContentLoaded');
+
+    const postMessageBtn = document.querySelector('#btn-send');
+    if (!postMessageBtn) return;
+
+    postMessageBtn.onclick = evt => {
+      evt.preventDefault();
+
+      if (!isSupportServiceWorker) {
+        throw new Error('[app] This browser does not support ServiceWorker.');
+      }
+
+      /*
+      // send message to service worker
+      navigator.serviceWorker.controller.postMessage({
+        msg: 'this is message from page'
+      });
+      */
+
+      // use message channel
+      if (!isSupportMessageChannel) {
+        throw new Error('[app] This browser does not support MessageChannel.');
+      }
+
+      const msgChannel = new MessageChannel();
+      msgChannel.port1.onmessage = function(evt) {
+        const data = evt.data;
+        console.log('[app] data received by port1 of message channel :', data);
+
+        if (!data) return;
+
+        switch (data.action) {
+          case 'getClientNum':
+            console.log('[app] result of "getClientsNum" action :', data.value);
+            break;
+        }
+      };
+
+      // send message with action, a port of message channel
+      navigator.serviceWorker.controller.postMessage(
+        {
+          action: 'getClientsNum'
+        },
+        [msgChannel.port2]
+      );
+    };
+  });
+})();
---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile('index.html');
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
