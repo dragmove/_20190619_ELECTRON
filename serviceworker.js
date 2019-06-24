ed6c3ea22e16c6ecd45be838d520765cfcc9b62f
김현석 <dragmove@naver.com>
2019-06-24 14:20:39 +0900

    feat: Translating serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -7,11 +7,8 @@ self.addEventListener('install', evt => {
   // https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle#skip_the_waiting_phase
   console.log('[sw] self.clients :', self.clients);
 
-  // postpone trigger 'install' evt until resolve promise.
-  // evt.waitUntil( promise );
-
   /*
-  // postpone 'install' evt until finishing some files caching.
+  // 특정 파일들의 caching 이 완료될 때까지 'install' event 의 발생을 지연시킨다.
   evt.waitUntil(
     caches.open('gih-cache').then(function(cache) {
       return cache.add('/index-offline.html')
@@ -24,24 +21,23 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
   console.log('[sw] activate event. service worker activated. evt :', evt);
 
-  // 서비스워커가 최초로 설치되면서 install 이벤트와 activate 이벤트가 발생했더라도,
-  // 곧바로 서비스워커가 제어하고 있는 client 는 찾을 수 없다.
+  // 서비스워커가 최초로 설치되면서 install 이벤트와 activate 이벤트가 발생했더라도, 그 즉시 서비스워커가 제어하고 있는 client 는 찾을 수 없다.
   // 새로 고침 이후부터 서비스워커가 client 들을 제어할 수 있다.
-  /*
-  postAllClients(clients => {
-    console.log(`[sw] post 'skipWaitingComplete' message to all clients. clients :`, clients);
-
-    clients.forEach(client => {
-      client.postMessage({ action: 'skipWaitingComplete' });
-    });
-  });
-  */
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/onfetch
 self.addEventListener('fetch', function(evt) {
   // console.log('Fetch request for :', evt.request.url);
+  // # payload response
+  // evt.respondWith(fetch(evt.request));
   /*
+  // # replace response with new fetch
+  if (evt.request.url.includes('/img/logo.png')) {
+    evt.respondWith(fetch('/img/logo-flipped.png'));
+  }
+  */
+  /*
+  // # detect fetch error and replace response
   evt.respondWith(
     fetch(evt.request).catch(function() {
       return new Response('[sw] Welcome to the offline mode');
@@ -49,16 +45,8 @@ self.addEventListener('fetch', function(evt) {
     })
   );
   */
-  // payload response
-  // evt.respondWith(fetch(evt.request));
   /*
-  // replace response with new fetch
-  if (evt.request.url.includes('/img/logo.png')) {
-    evt.respondWith(fetch('/img/logo-flipped.png'));
-  }
-  */
-  /*
-  // replace response
+  // # replace response
   if (evt.request.url.includes('bootstrap.min.css')) {
     evt.respondWith(
       new Response('background-color: #ff0;', {
@@ -79,16 +67,17 @@ self.addEventListener('message', evt => {
   console.log('[sw] message event :', evt);
 
   const data = evt.data,
-    port = evt.ports[0]; // receive port2 of message channel from client page
+    port = evt.ports[0]; // client의 MessageChannel의 port2를 전달 받는다.
 
   console.log(`[sw] evt.data from client ${evt.source.id} :`, data);
-  console.log('[sw] evt.source :', evt.source); // { focused: true, frameType: "top-level", id: "893e3eef-1f4b-4eec-b1ae-e7a68eb70ffc", type: "window", url: "http://localhost:8443/", visibilityState: "visible" }
+  console.log('[sw] evt.source :', evt.source); // e.g: { focused: true, frameType: "top-level", id: "893e3eef-1f4b-4eec-b1ae-e7a68eb70ffc", type: "window", url: "http://localhost:8443/", visibilityState: "visible" }
 
   switch (data.action) {
     case 'getClientsNum':
       self.clients.matchAll().then(_clients => {
         console.log('[sw] clients :', _clients);
 
+        // 현재 서비스워커가 제어하고 있는 client 갯수를 client 측에 전달한다.
         port.postMessage({
           action: 'getClientNum',
           value: _clients.length,
@@ -99,10 +88,13 @@ self.addEventListener('message', evt => {
 
     case 'skipWaiting':
       self.skipWaiting().then(() => {
-        console.log('[sw] complete skipWaiting()');
+        console.log('[sw] resolve skipWaiting() promise');
 
         postAllClients(clients => {
-          console.log(`[sw] post 'skipWaitingComplete' message to all clients. clients :`, clients);
+          console.log(
+            `[sw] 모든 client 들에게 'skipWaitingComplete' action을 postMessage로 전달한다. clients :`,
+            clients
+          );
 
           clients.forEach(client => {
             client.postMessage({ action: 'skipWaitingComplete' });
@@ -113,14 +105,14 @@ self.addEventListener('message', evt => {
   }
 
   /*
-  // send message to one client who posted message
+  // message를 전달한 한 client에게 message를 전달한다.
   this.self.clients.get(evt.source.id).then(client => {
     client.postMessage(`client.id from clients.get(evt.source.id): ${evt.source.id}`);
   });
   */
 
-  // send message to all clients
   /*
+  // 모든 client들에게 message를 전달한다.
   self.clients.matchAll().then(clients => {
     clients.forEach(client => {
       // console.log('[sw] client.id :', client.id);
@@ -128,13 +120,6 @@ self.addEventListener('message', evt => {
     });
   });
   */
-});
-
-/*
- * push process
- */
-self.addEventListener('push', function() {
-  console.log('[sw] push');
 });
 
 function postAllClients(resolveCallback) {
